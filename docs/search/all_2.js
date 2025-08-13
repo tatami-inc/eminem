3ee643cdd1d21cca245f083b1c15ab3408210677
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-08-13 16:44:17 +0000

    Deploying to gh-pages from @ tatami-inc/eminem@840b086a12881e3f53b8ce5651b150f81bdc997d 🚀

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['c_0',['Parse Matrix Market files in C++',['../index.html',1,'']]],
-  ['chunkedbufferreader_1',['ChunkedBufferReader',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1ChunkedBufferReader.html',1,'byteme::ChunkedBufferReader'],['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1ChunkedBufferReader.html#a7e895a11c107750473d5e0ab7d3cad76',1,'byteme::ChunkedBufferReader::ChunkedBufferReader()']]],
-  ['chunkedbufferreader_2ehpp_2',['ChunkedBufferReader.hpp',['https://ltla.github.io/byteme/byteme.tag/ChunkedBufferReader_8hpp.html',1,'']]],
-  ['compression_5flevel_3',['compression_level',['https://ltla.github.io/byteme/byteme.tag/structbyteme_1_1GzipFileWriterOptions.html#a9072c378aefb629c314f80ff8b77a964',1,'byteme::GzipFileWriterOptions::compression_level'],['https://ltla.github.io/byteme/byteme.tag/structbyteme_1_1ZlibBufferWriterOptions.html#a31ad029c8bd29c6edfe6e1228ee2aab7',1,'byteme::ZlibBufferWriterOptions::compression_level']]]
+  ['cap_1',['cap',['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#a6eed452f40aae5af9b40b75aa3105b56',1,'byteme']]],
+  ['check_5fbuffer_5fsize_2ehpp_2',['check_buffer_size.hpp',['https://ltla.github.io/byteme/byteme.tag/check__buffer__size_8hpp.html',1,'']]],
+  ['chunkedbufferreader_3',['ChunkedBufferReader',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1ChunkedBufferReader.html',1,'byteme::ChunkedBufferReader'],['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1ChunkedBufferReader.html#a7e895a11c107750473d5e0ab7d3cad76',1,'byteme::ChunkedBufferReader::ChunkedBufferReader()']]],
+  ['chunkedbufferreader_2ehpp_4',['ChunkedBufferReader.hpp',['https://ltla.github.io/byteme/byteme.tag/ChunkedBufferReader_8hpp.html',1,'']]],
+  ['compression_5flevel_5',['compression_level',['https://ltla.github.io/byteme/byteme.tag/structbyteme_1_1GzipFileWriterOptions.html#a9072c378aefb629c314f80ff8b77a964',1,'byteme::GzipFileWriterOptions::compression_level'],['https://ltla.github.io/byteme/byteme.tag/structbyteme_1_1ZlibBufferWriterOptions.html#a31ad029c8bd29c6edfe6e1228ee2aab7',1,'byteme::ZlibBufferWriterOptions::compression_level']]]
 ];
