8767dc7b10df4ab08d3aa610e26b3ea40214bd04
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-03-24 04:03:40 +0000

    Deploying to gh-pages from @ tatami-inc/eminem@d8dd8a463c94d588793b1080d6ce5c7bc3fa2d4d 🚀

--- docs/search/all_6.js
+++ docs/search/all_6.js
@@ -3,6 +3,7 @@ var searchData=
   ['in_20c_0',['Parse Matrix Market files in C++',['../index.html',1,'']]],
   ['is_5fgzip_1',['is_gzip',['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#acb08f1cce34b8b237e3174b307a34178',1,'byteme::is_gzip(const unsigned char *buffer, std::size_t n)'],['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#a079575e91360061a7495713a7e872258',1,'byteme::is_gzip(const char *path)']]],
   ['is_5fzlib_2',['is_zlib',['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#ad3b16db0612e6afdf861519e51d71653',1,'byteme']]],
-  ['istreamreader_3',['IstreamReader',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1IstreamReader.html',1,'byteme::IstreamReader&lt; class Pointer_ &gt;'],['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1IstreamReader.html#a9ec92866979a86aa6a654068b734224c',1,'byteme::IstreamReader::IstreamReader()']]],
-  ['istreamreader_2ehpp_4',['IstreamReader.hpp',['https://ltla.github.io/byteme/byteme.tag/IstreamReader_8hpp.html',1,'']]]
+  ['is_5fzlib_5for_5fgzip_3',['is_zlib_or_gzip',['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#a897b54b7e8ab0eb00006d7d11b7a9563',1,'byteme']]],
+  ['istreamreader_4',['IstreamReader',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1IstreamReader.html',1,'byteme::IstreamReader&lt; class Pointer_ &gt;'],['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1IstreamReader.html#a9ec92866979a86aa6a654068b734224c',1,'byteme::IstreamReader::IstreamReader()']]],
+  ['istreamreader_2ehpp_5',['IstreamReader.hpp',['https://ltla.github.io/byteme/byteme.tag/IstreamReader_8hpp.html',1,'']]]
 ];
--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['is_5fgzip_0',['is_gzip',['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#acb08f1cce34b8b237e3174b307a34178',1,'byteme::is_gzip(const unsigned char *buffer, std::size_t n)'],['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#a079575e91360061a7495713a7e872258',1,'byteme::is_gzip(const char *path)']]],
   ['is_5fzlib_1',['is_zlib',['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#ad3b16db0612e6afdf861519e51d71653',1,'byteme']]],
-  ['istreamreader_2',['IstreamReader',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1IstreamReader.html#a9ec92866979a86aa6a654068b734224c',1,'byteme::IstreamReader']]]
+  ['is_5fzlib_5for_5fgzip_2',['is_zlib_or_gzip',['https://ltla.github.io/byteme/byteme.tag/namespacebyteme.html#a897b54b7e8ab0eb00006d7d11b7a9563',1,'byteme']]],
+  ['istreamreader_3',['IstreamReader',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1IstreamReader.html#a9ec92866979a86aa6a654068b734224c',1,'byteme::IstreamReader']]]
 ];
