5f54fee1359b18e710f0aa886e622fe6e57f9b4c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-05-30 09:17:29 +0000

    Deploying to gh-pages from @ tatami-inc/eminem@28111d1a6a0d8476a6510944adcc9ddfba332832 🚀

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['eminem_0',['eminem',['../namespaceeminem.html',1,'']]],
   ['eminem_2ehpp_1',['eminem.hpp',['../eminem_8hpp.html',1,'']]],
-  ['extract_2',['extract',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1BufferedReader.html#a1c15c13a980c5f5a99d6a780317c413e',1,'byteme::BufferedReader']]]
+  ['extract_2',['extract',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1BufferedReader.html#a1c15c13a980c5f5a99d6a780317c413e',1,'byteme::BufferedReader']]],
+  ['extract_5funtil_3',['extract_until',['https://ltla.github.io/byteme/byteme.tag/classbyteme_1_1BufferedReader.html#a71b85149e3a02321f0568d845c0280ee',1,'byteme::BufferedReader']]]
 ];
